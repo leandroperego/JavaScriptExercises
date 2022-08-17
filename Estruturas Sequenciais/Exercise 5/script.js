d43863f0d96b9bccf4d2fe450b69243f727c40c1
Leandro Perego <leperego@hotmail.com>
2022-08-17 14:17:07 -0300

    Exercise 5 - Version 1

---
+++ Estruturas Sequenciais/Exercise 5/script.js
@@ -0,0 +1,16 @@
+/*
+	Enunciado:
+
+	Um motorista deseja abastecer um valor X em reais, de combustível. 
+	Escreva um algoritmo para ler o preço do litro do combustível e o valor que o motorista deseja abastecer. 
+	Em seguida, informe quantos litros foram abastecidos.
+	
+	Créditos: Exercícios propostos pelo prof. Luiz Paulo Maia - Instituto Infnet
+*/
+
+//Versão 1
+const quantia = Number(prompt("Informe a quantia disponivel em R$:"));
+
+const valorLitro = Number(prompt("Qual o valor do Litro em R$:"));
+
+console.log(`Total de Litros de combustivel abastecido: ${quantia / valorLitro} litros.`);
