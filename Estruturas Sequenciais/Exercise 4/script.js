02607edcb8612cb378e62610f3c2dc8c3a1f3965
Leandro Perego <leperego@hotmail.com>
2022-08-17 14:05:15 -0300

    Exercise 4 - Version 1

---
+++ Estruturas Sequenciais/Exercise 4/script.js
@@ -0,0 +1,14 @@
+/*
+	Enunciado:
+
+	Crie um algoritmo que recebe um valor de temperatura em Celsius e o converte para Fahrenheit. 
+	
+	Créditos: Exercícios propostos pelo prof. Luiz Paulo Maia - Instituto Infnet
+*/
+
+//Versão 1
+const tempC = Number(prompt("Informe a temperatura em Celsius:"));
+
+const faren = tempC * 9 / 5 + 32;
+
+console.log(`${tempC}ºC => ${faren}ºF`);
