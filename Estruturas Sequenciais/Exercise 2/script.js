f97e8772216e486fedf901adcad0318adbc2ccbb
Leandro Perego <leperego@hotmail.com>
2022-08-17 13:51:21 -0300

    Exercise 2 - Version 1

---
+++ Estruturas Sequenciais/Exercise 2/script.js
@@ -0,0 +1,22 @@
+/*
+	Enunciado:
+
+	Recrie o algoritmo de cálculo de média das notas, mas, desta vez, calcule a média ponderada, 
+	sabendo que a primeira nota possui peso 1, a segunda nota possui peso 2 e a terceira nota possui peso 3.
+	
+	Créditos: Exercícios propostos pelo prof. Luiz Paulo Maia - Instituto Infnet
+*/
+
+//Versão 1
+const nota1 = Number(prompt("Informe nota 1:"));
+const peso1 = 1;
+
+const nota2 = Number(prompt("Informe nota 2:"));
+const peso2 = 2;
+
+const nota3 = Number(prompt("Informe nota 3:"));
+const peso3 = 3;
+
+const mediaPond = (nota1*peso1 + nota2*peso2 + nota3*peso3) / (peso1 + peso2 + peso3);
+
+console.log(`Média Ponderada: ${mediaPond.toFixed(2)}`);
