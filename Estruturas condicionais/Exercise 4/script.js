fe24eae06b3b933d4d32df24951867d89d8da12b
Leandro Perego <leperego@hotmail.com>
2022-08-18 21:23:09 -0300

    Exercise 4 - Version 1

--- Estruturas condicionais/Exercise 4/script.js
+++ Estruturas condicionais/Exercise 4/script.js
@@ -10,3 +10,19 @@
 */
 
 //Versão 1
+
+let numA = Number(prompt("Informe o primeiro número:"));
+let numB = Number(prompt("Informe o segundo número:"));
+let numC = Number(prompt("Informe o terceiro número:"));
+
+let resultado;
+
+if (numA > numB && numA > numC){
+	resultado = numA;
+}else if (numB > numA && numB > numC) {
+	resultado = numB;
+}else{
+	resultado = numC;
+}
+
+console.log(`O maior número informado dos números ${numA}, ${numB} e ${numC} é ${resultado}.`);
