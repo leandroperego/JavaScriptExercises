8403c598249d3c60cdeb56a3acdf42ba8c23e621
Leandro Perego <leperego@hotmail.com>
2022-08-18 22:18:06 -0300

    Exercise 5 - Version 1

--- Estruturas condicionais/Exercise 5/script.js
+++ Estruturas condicionais/Exercise 5/script.js
@@ -7,3 +7,52 @@
 */
 
 //Versão 1
+
+let numA = Number(prompt("Informe o primeiro número:"));
+let numB = Number(prompt("Informe o segundo número:"));
+let numC = Number(prompt("Informe o terceiro número:"));
+
+let maior, meio, menor;
+
+if (numA >= numB && numA >= numC){
+	maior = numA;
+	if (numB >= numC){
+		meio = numB;
+		menor = numC;
+	} else{
+		meio = numC;
+		menor = numB;
+	}
+}
+
+if(numB > numA && numB > numC ){
+	maior = numB;
+	if (numA >= numC){
+		meio = numA;
+		menor = numC;
+	}else {
+		meio = numC;
+		menor = numA;
+	}
+}
+
+if(maior == undefined){
+	maior = numC;
+	if(numA >= numB){
+		meio = numA;
+		menor = numB;
+	} else{
+		meio = numB;
+		menor = numA;
+	}
+}
+
+console.log("Número digitados:\n"
+	+ "1º: " + numA + "\n"
+	+ "2º: " + numB + "\n"
+	+ "3º: " + numC);
+
+console.log("Números em ordem crescente:\n"
+	+ "1º: " + menor + "\n"
+	+ "2º: " + meio + "\n"
+	+ "3º: " + maior);
