105bdf5c544c9831f068c68c2caf1d37e051952f
Leandro Perego <leperego@hotmail.com>
2022-08-18 21:14:00 -0300

    Exercise 3 - Version 1

--- Estruturas condicionais/Exercise 3/script.js
+++ Estruturas condicionais/Exercise 3/script.js
@@ -7,3 +7,16 @@
 */
 
 //Versão 1
+
+let numA = Number(prompt("Digite primeiro número:"));
+let numB = Number(prompt("Digite segundo número:"));
+
+let resultado;
+
+if(numA > numB){
+	resultado = numA;
+} else{
+	resultado = numB
+}
+
+console.log("O maior número digitado foi o " + resultado);
