e8a552051505109af55eeb7aa8ed72dd92894106
Leandro Perego <leperego@hotmail.com>
2022-08-18 21:05:58 -0300

    Exercise 2 - Version 1

--- Estruturas condicionais/Exercise 2/script.js
+++ Estruturas condicionais/Exercise 2/script.js
@@ -7,3 +7,14 @@
 */
 
 //Versão 1
+
+let numero = Number(prompt("Digite um número pra saber se ele é par ou impar:"));
+let resultado;
+
+if (numero % 2 == 0){
+	resultado = 'par';
+} else{
+	resultado = 'impar';
+}
+
+console.log("O número " + numero + " é " + resultado);
