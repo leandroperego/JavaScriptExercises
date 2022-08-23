c4f72ba2ad111121311e29813936b3d0c56ba69f
Leandro Perego <leperego@hotmail.com>
2022-08-23 20:29:00 -0300

    Exercise 8 - Version 1

--- Estruturas condicionais/Exercise 8/script.js
+++ Estruturas condicionais/Exercise 8/script.js
@@ -9,3 +9,18 @@
 */
 
 //Versão 1
+
+let octeto = Math.floor(+prompt("Digite o primeiro octeto do seu IP:"));
+let classe;
+
+if (octeto >= 240) classe = "E";
+else if (octeto >= 224) classe = "D";
+else if (octeto >= 192) classe = "C";
+else if (octeto >= 128) classe = "B";
+else if(octeto >= 0) classe = "A";
+else classe = "Número negativo não é válido";
+
+if (classe.length > 1) { // se o tamanho da string classe for > 1 (não ser apenas uma letra), retorna Numero negativo não é valido
+	console.log(classe);
+} 
+else console.log(`Considerando o octeto ${octeto}, ele é considerado da classe ${classe}.`);
