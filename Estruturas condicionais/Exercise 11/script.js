f7240f0fa11f8c01b04173a2379df8ef8799ab1b
Leandro Perego <leperego@hotmail.com>
2022-08-24 17:33:02 -0300

    Exercise 11 - Version 1

--- Estruturas condicionais/Exercise 11/script.js
+++ Estruturas condicionais/Exercise 11/script.js
@@ -1,8 +1,21 @@
 /*
 	Enunciado:
 
+	Vide index.html
 
 	Créditos: Exercícios propostos pelo prof. Luiz Paulo Maia - Instituto Infnet
 */
 
 //Versão 1
+
+let distancia = +prompt("Informe a distância do lançamento:");
+let ponto;
+if (distancia <= 800) {
+	ponto = 1;
+} else if(distancia <= 1400){
+	ponto = 2;
+} else if(distancia < 2000){
+	ponto = 3;
+}
+
+console.log(ponto);
