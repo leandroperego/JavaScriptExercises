03f5e7c30945538eb5c32671889f5b8cbf0d6456
Leandro Perego <leperego@hotmail.com>
2022-08-24 10:52:13 -0300

    Exercise 9 - Version 1

--- Estruturas condicionais/Exercise 9/script.js
+++ Estruturas condicionais/Exercise 9/script.js
@@ -8,3 +8,50 @@
 */
 
 //Versão 1
+
+let numeroMes = +prompt("Digite o número de um mês do ano:");
+let mes;
+
+if (numeroMes > 12 || numeroMes < 1) {
+	console.log("Número inválido. Tente novamente.");
+} else {
+	switch (numeroMes){
+		case 1 :
+			mes = `Janeiro`;
+			break;
+		case 2 :
+			mes = `Fevereiro`;
+			break;
+		case 3 :
+			mes = `Março`;
+			break;
+		case 4 :
+			mes = `Abril`;
+			break;
+		case 5 :
+			mes = `Maio`;
+			break;
+		case 6 :
+			mes = `Junho`;
+			break;
+		case 7 :
+			mes = `Julho`;
+			break;
+		case 8 :
+			mes = `Agosto`;
+			break;
+		case 9 :
+			mes = `Setembro`;
+			break;
+		case 10 :
+			mes = `Outubro`;
+			break;
+		case 11 :
+			mes = `Novembro`;
+			break;
+		case 12 :
+			mes = `Dezembro`;
+			break;		
+	}
+	console.log(`O mês ${numeroMes} representa o mês de ${mes}.`);
+}
