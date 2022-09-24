2a2b93eeba9125782c91244f21b63f46a168d5d1
Leandro Perego <leperego@hotmail.com>
2022-09-24 20:55:38 -0300

    Exercise 9 - É número primo?

--- Estruturas de repetição/Exercise 9/script.js
+++ Estruturas de repetição/Exercise 9/script.js
@@ -8,3 +8,22 @@
 */
 
 //Versão 1
+
+let numero = +prompt("Informe um número para saber se ele é primo ou não:");
+
+console.log(ehPrimo(numero));
+
+function ehPrimo(numero){
+	for (var i = 2; i < numero; i++) {
+		if (numero % i == 0){
+			return (`O número ${numero} não é primo.`);
+		} 
+	}
+	if (numero > 1){
+		return (`O número ${numero} é primo.`);
+	} else if(numero == 1 || numero == 0){
+		return (`O número ${numero} não é primo.`);
+	} else{
+		return (`Número negativo.`);
+	}
+}
