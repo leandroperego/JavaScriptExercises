ad23b3ae73adb40744f8161dd26896e72d2f7073
Leandro Perego <leperego@hotmail.com>
2022-09-24 21:35:44 -0300

    Exercise 10 - Lista de números é primo ou não

--- Estruturas de repetição/Exercise 10/script.js
+++ Estruturas de repetição/Exercise 10/script.js
@@ -7,3 +7,37 @@
 */
 
 //Versão 1
+
+let continuar = true;
+let arrayNumeros = [];
+
+alert("Você irá informar alguns valores aleatórios e depois mostrarei cada número lido e se ele é primo ou não.");
+
+let numero = +prompt("Digite um número:");
+while (numero != 0){
+
+	arrayNumeros.push(numero);
+	numero = +prompt("Digite outro número: Caso queira interromper, digite 0");
+}
+
+for (var i = 0; i < arrayNumeros.length; i++) {
+	if (ehPrimo(arrayNumeros[i])){
+		console.log(`${arrayNumeros[i]} é primo.`);
+	} else{
+		console.log(`${arrayNumeros[i]} não é primo.`);
+	}
+}
+
+function ehPrimo(numero){
+
+	if (numero == 1){
+		return false;
+	}
+	
+	for (var i = 2; i < numero; i++) {
+		if (numero % i == 0){
+			return false; 
+		}
+	}
+	return true;
+}
