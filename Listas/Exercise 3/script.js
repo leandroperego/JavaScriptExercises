2c2adce709b8af7203d3f267627406d025959d53
Leandro Perego <leperego@hotmail.com>
2022-10-27 15:04:35 -0300

    Metodo reverse() arrays

--- Listas/Exercise 3/script.js
+++ Listas/Exercise 3/script.js
@@ -7,3 +7,17 @@
 */
 
 //Versão 1
+let lista = [];
+let numero;
+
+do{
+	numero = +prompt("Informe um número para adicionar a lista: (Caso queira sair, digite 0):");
+
+	if (numero != 0){
+		lista.push(numero);
+	}
+
+} while (numero != 0);
+
+console.log(`Ordem informada: ${lista.join(" - ")}`);
+console.log(`Ordem reversa: ${lista.reverse().join(" - ")}`);
