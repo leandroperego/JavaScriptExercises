71810c8c0c17d24c87edce2bc18804819586fdf8
Leandro Perego <leperego@hotmail.com>
2022-10-27 14:55:46 -0300

    Maior e menor numero de uma lista

--- Listas/Exercise 2/script.js
+++ Listas/Exercise 2/script.js
@@ -11,3 +11,46 @@ Lembre-se de que, na primeira vez que o laço é executado, o maior e o menor n�
 */
 
 //Versão 1
+// let maior, menor;
+// let numero;
+// do{
+// 	numero = +prompt("Informe um número para adicionar a lista: (Caso queira sair, digite 0):");
+
+// 	if (numero == 0){
+// 		break;
+// 	}
+// 	if(maior == undefined && menor == undefined){
+// 		maior = numero;
+// 		menor = numero;
+// 	}
+
+// 	if (numero > maior){
+// 		maior = numero;
+// 	}
+
+// 	if (numero < menor){
+// 		menor = numero;
+// 	}
+
+// } while(true);
+
+// console.log(`O maior numero digitado foi ${maior}.`);
+// console.log(`O menor numero digitado foi ${menor}.`);
+
+//VERSAO COM LISTA:
+let lista = [];
+let numero;
+
+do{
+	numero = +prompt("Informe um número para adicionar a lista: (Caso queira sair, digite 0):");
+
+	if(numero == 0){
+		break;
+	}
+
+	lista.push(numero);
+
+} while(true);
+
+console.log(`O maior numero digitado foi ${Math.max(...lista)}.`);
+console.log(`O menor numero digitado foi ${Math.min(...lista)}.`);
