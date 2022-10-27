c00f6157c83d8ff5d0a779ed40cef3a70a23f9f1
Leandro Perego <leperego@hotmail.com>
2022-10-27 14:42:13 -0300

    Exercicio somar e multiplicar numeros da lista

--- Listas/Exercise 1/script.js
+++ Listas/Exercise 1/script.js
@@ -6,3 +6,22 @@
 */
 
 //Versão 1
+const lista = [];
+let soma = 0;
+let multiplicacao = 1;
+let numero;
+
+do{
+	numero = +prompt("Informe um número para adicionar a lista: (Caso queira sair, digite 0):");
+
+	if(numero != 0){
+		lista.push(numero);
+		soma += numero;
+		multiplicacao *= numero;
+	}
+
+} while(numero != 0);
+
+console.log(`Números digitados: ${lista.join(" - ")}`);
+console.log(`A soma desses números é igual a ${soma}.`);
+console.log(`O produtos desses números é igual a ${multiplicacao}.`);
