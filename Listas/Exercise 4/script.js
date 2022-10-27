2518ccaed931586b4f838334482c368444c1f17e
Leandro Perego <leperego@hotmail.com>
2022-10-27 15:16:47 -0300

    Funcao Set() para tirar valores repetidos de uma  lista

--- Listas/Exercise 4/script.js
+++ Listas/Exercise 4/script.js
@@ -7,3 +7,21 @@
 */
 
 //Versão 1
+
+let lista = [];
+let numero;
+
+do{
+	numero = +prompt("Informe um número para adicionar a lista: (Caso queira sair, digite 0):");
+
+	if (numero != 0){
+		lista.push(numero);
+		lista.push(numero);
+	}
+
+} while (numero != 0);
+
+console.log(`Lista informada com valores duplicados: ${lista.join(" - ")}`);
+
+let listaSemRepeticoes = [...new Set(lista)];
+console.log(`Lista sem valores duplicados com o metodo x: ${listaSemRepeticoes.join(" - ")}`);
