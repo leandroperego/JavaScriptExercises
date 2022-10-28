d2774a665d760cc9d7976ff31720c7ff5195d610
Leandro Perego <leperego@hotmail.com>
2022-10-28 19:35:42 -0300

    Saber o indice de um elemento em uma array

--- Listas/Exercise 7/script.js
+++ Listas/Exercise 7/script.js
@@ -8,3 +8,11 @@
 */
 
 //Versão 1
+let lista = [1,2,3,4,5,6,7,8,9,10];
+
+let tela = lista.join("-");
+tela += "\n\nInforme o numero da lista que deseja saber o indice dele na lista:";
+let numeroDesejado = +prompt(tela);
+
+let indexDoNumero = lista.indexOf(numeroDesejado);
+alert("O numero " + numeroDesejado + " esta no indice " + indexDoNumero);
