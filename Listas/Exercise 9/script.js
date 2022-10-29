34122b4fd9ee1263504f19c37deb3d9f6c2954a4
Leandro Perego <leperego@hotmail.com>
2022-10-29 10:53:47 -0300

    Exercicio 09 - Interpolando dados com arrays dif

--- Listas/Exercise 9/script.js
+++ Listas/Exercise 9/script.js
@@ -17,3 +17,93 @@ Após povoar as listas, o algoritmo deve fornecer um relatório informando:
 */
 
 //Versão 1
+
+let listaNomes = [];
+let listaIdade = [];
+let listaSexo = [];
+
+for (var i = 0; i < 5; i++) {
+	let tela = "PESSOA " + (i+1) + ":\n";
+	let nome = prompt(tela + "Nome:");
+	let idade = prompt(tela + "Idade:");
+	let sexo = prompt(tela + "Sexo: (M/F)");
+
+	listaNomes.push(nome);
+	listaIdade.push(idade);
+	listaSexo.push(sexo);
+}
+
+console.log(`Foram informados ${qtdeSexo("F")} mulheres.`);
+console.log(`Foram informados ${qtdeSexo("M")} homens.`);
+console.log(`A média de idade das mulheres é ${mediaIdade("F")} anos.`);
+console.log(`A média de idade dos homens é ${mediaIdade("M")} anos.`);
+console.log(`O homem mais velho é o ${maisVelho("M")}.`);
+console.log(`A mulher mais jovem é a ${maisNovo("F")}.`);
+console.log(`A pessoa mais velha informado é ${maisVelho()}.`);
+console.log(`A pessoa mais jovem informado é ${maisNovo()}.`);
+
+
+//-----------------------FUNCTIONS-----------------------
+
+function qtdeSexo(sexo){
+	let contador = 0;
+	for (var i = 0; i < listaSexo.length; i++) {
+		if (listaSexo[i] == sexo){
+			contador++;
+		}
+	}
+	return contador;
+}
+
+function mediaIdade(sexo){
+	let listaComOsIndices = listaComIndices(sexo);
+	let soma = 0;
+	let qtde = listaComOsIndices.length;
+
+	for (var i = 0; i < listaComOsIndices.length; i++) {
+		soma += +listaIdade[listaComOsIndices[i]];
+	}
+
+	return soma / qtde;
+}
+
+function maisVelho(sexo){
+	let listaComOsIndices = listaComIndices(sexo);
+
+	let maisVelho = listaIdade[listaComOsIndices[0]];
+
+	for (var i = 1; i < listaComOsIndices.length; i++) {
+
+		if (maisVelho < listaIdade[listaComOsIndices[i]]){
+			maisVelho = listaIdade[listaComOsIndices[i]];
+		}
+	}
+
+	return listaNomes[listaIdade.indexOf(maisVelho)];
+}
+
+function maisNovo(sexo){
+	let listaComOsIndices = listaComIndices(sexo);
+
+	let maisNovo = listaIdade[listaComOsIndices[0]];
+
+	for (var i = 1; i < listaComOsIndices.length; i++) {
+
+		if (maisNovo > listaIdade[listaComOsIndices[i]]){
+			maisNovo = listaIdade[listaComOsIndices[i]];
+		}
+	}
+
+	return listaNomes[listaIdade.indexOf(maisNovo)];
+}
+
+function listaComIndices(sexo){
+	let lista = [];
+
+	for (var i = 0; i < listaSexo.length; i++) {
+		if (listaSexo[i] == sexo || sexo == undefined){
+			lista.push(i);
+		}
+	}
+	return lista;
+}
